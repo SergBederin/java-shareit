7eed85d8556fcf35248ce40e89a482d5401623ef
SergBederin <gres.xedny@yandex.ru>
2023-07-03 00:26:42 +0300

    feat:добавить Docker

---
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.request.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * TODO Sprint add-item-requests.
+ */
+@Data
+@Builder
+public class ItemRequestDto {
+    private Long id;
+    @NotBlank
+    @NotEmpty
+    private String description;
+    private Long requestor;
+    private LocalDateTime created;
+    private List<ItemDto> items;
+}
